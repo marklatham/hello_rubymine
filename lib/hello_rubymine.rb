4093736ce53666d0d184a3454e6a618204556247
Mark Latham <mark7latham@gmail.com>
2021-08-26 13:15:53 -0700

    Edited gem code and gemspec.

--- lib/hello_rubymine.rb
+++ lib/hello_rubymine.rb
@@ -4,5 +4,7 @@ require_relative "hello_rubymine/version"
 
 module HelloRubymine
   class Error < StandardError; end
-  # Your code goes here...
+  def self.greet(name)
+    puts "Hello, #{name}! I'm Ruby!"
+  end
 end
